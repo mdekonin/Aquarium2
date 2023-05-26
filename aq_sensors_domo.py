6912b95a6bc4baea25ae3eeff60ad0bc6fdbf83a
Michael de Koning <mdekonin@bigpond.net.au>
2023-05-26 15:04:33 +1000

    Sending Data to Domo via MQTT

--- aq_sensors_domo.py
+++ aq_sensors_domo.py
@@ -15,30 +15,32 @@ import time
 from datetime import datetime
 from w1thermsensor import W1ThermSensor, Sensor, Unit
 import syslog
-import json
+import ssl
 
-MQTT_broker = "192.168.1.241"
-MQTT_port = 1883
+MQTT_broker = "domo.import.domo.com"
+MQTT_port = 8883
 MQTT_topic = "Temperature"
-INTERVAL = 240
+INTERVAL = 300
 
 def on_connect(client, userdata, flags, rc):
+    print("Connection retunred " + str(rc))
     if rc == 0:
         print("Connected to broker")
         global Connected                #Use global variable
         Connected = True                #Signal connection 
     else:
-        error_msg = "Connection failed with return code : " + str(rc)
-        print(error_msg)
-        syslog.syslog(error_msg)
+        print("Connection failed")
 
 def get_Temp(client):
     for sensor in W1ThermSensor.get_available_sensors([Sensor.DS18B20]):
         tempC = sensor.get_temperature()
-        msg_payload = {"timestamp" : str(datetime.now()), "sensor_id" : sensor.id, "sensor_type" : "DS18B20", "temp_C" : tempC}
-        msg_json = json.dumps(msg_payload)
-        syslog.syslog(msg_json)
-        client.publish(MQTT_topic, msg_json)
+        tag1 = "sensor_id=" + str(sensor.id)
+        tag2 = "sensor_type=DS18B20"
+        value = str(tempC)
+        msg = str(datetime.now()) + "," + tag1 + "," + tag2 + "," + value
+        print(msg)
+        syslog.syslog(msg)
+        client.publish(MQTT_topic, msg)
     return()
 
 def get_pH():
@@ -50,17 +52,25 @@ if __name__ == '__main__':
     Connected = False                                   #global variable for the state of the connection
     broker_address= MQTT_broker
     port = MQTT_port
-    user = "mdekonin"
-    password = "Ru$$ia2day00"
-    
+    #user = "yourUser"
+    #password = "yourPassword"
     client = mqttClient.Client()                        #create new instance
-    client.username_pw_set(user, password=password)    #set username and password
+    print(client)
+    #client.username_pw_set(user, password=password)    #set username and password
     client.on_connect= on_connect                      #attach function to callback
+    print("tls_set")
+    #client.tls_set("/etc/ssl/certs/ca-certificates.crt", tls_version=ssl.PROTOCOL_TLSv1_2)
+    client.tls_set("/usr/local/share/ca-certificates/ca.crt")    #, tls_version=ssl.PROTOCOL_TLSv1_2)
+    print("tls_insecure_set")
+    client.tls_insecure_set(True)
+    print("Connecting to MQTT broker")
     client.connect(broker_address, port=port)          #connect to broker
+    print("Entering client loop")
     client.loop_start()                                #start the loop
-    
+
     while Connected != True:    #Wait for connection
-        time.sleep(0.1)
+        print(". ", end="")
+        time.sleep(0.5)
     try :
         while True:
             get_Temp(client)
