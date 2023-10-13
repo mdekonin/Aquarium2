e9685a9d8fa6f4eed55dd6f504dfe56ceec5b3db
Michael de Koning <52337228+mdekonin@users.noreply.github.com>
2023-10-13 17:52:16 +1100

    Add files via upload

--- aq_sensors.py
+++ aq_sensors.py
@@ -9,6 +9,19 @@
 #
 # Phase 1. DS18B20
 # Phase 2. add MQTT client code to publish results
+#
+# to be done :
+# a. handling for sensor not ready error
+# Oct 12 11:44:56 aquarium python3[15227]:   File "/home/pi/aquarium2/2.02/aq_sensors.py", line 38, in get_Temp
+# Oct 12 11:44:56 aquarium python3[15227]:     tempC = sensor.get_temperature()
+# Oct 12 11:44:56 aquarium python3[15227]:   File "/usr/local/lib/python3.9/dist-packages/w1thermsensor/core.py", line 26>
+# Oct 12 11:44:56 aquarium python3[15227]:     raw_temperature_line = self.get_raw_sensor_strings()[1]
+# Oct 12 11:44:56 aquarium python3[15227]:   File "/usr/local/lib/python3.9/dist-packages/w1thermsensor/core.py", line 25>
+# Oct 12 11:44:56 aquarium python3[15227]:     raise SensorNotReadyError(self)
+# Oct 12 11:44:56 aquarium python3[15227]: w1thermsensor.errors.SensorNotReadyError: Sensor 0416c0f6b7ff is not yet ready>
+# Oct 12 11:44:56 aquarium systemd[1]: aq_sensors.service: Main process exited, code=exited, status=1/FAILURE
+# Oct 12 11:44:56 aquarium systemd[1]: aq_sensors.service: Failed with result 'exit-code'.
+# Oct 12 11:44:56 aquarium systemd[1]: aq_sensors.service: Consumed 17min 55.394s CPU time.
 
 import paho.mqtt.client as mqttClient
 import time
@@ -17,36 +30,60 @@ from w1thermsensor import W1ThermSensor, Sensor, Unit
 import syslog
 import json
 
-MQTT_broker = "192.168.1.241"
+MQTT_broker = "Pi-Hole"
 MQTT_port = 1883
 MQTT_topic = "Temperature"
 INTERVAL = 240
 
+def log(msg):
+    print(msg)
+    syslog.syslog(msg)
+
+
+# give everything a chance to start up
+def startup(seconds):
+    log("INFO : Pausing while system starts up")
+    for i in range(0,seconds):
+        print
+        time.sleep(1)
+    log("INFO : OK to go now")
+
+
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("Connected to broker")
+        log("INFO : Connected to MQTT broker " + MQTT_broker + " : " + str(MQTT_port))
         global Connected                #Use global variable
         Connected = True                #Signal connection 
     else:
-        error_msg = "Connection failed with return code : " + str(rc)
-        print(error_msg)
-        syslog.syslog(error_msg)
+        log("ERROR : Connection failed with return code : " + str(rc))
 
 def get_Temp(client):
+    log("DEBUG : entering get_temp")
     for sensor in W1ThermSensor.get_available_sensors([Sensor.DS18B20]):
-        tempC = sensor.get_temperature()
-        msg_payload = {"timestamp" : str(datetime.now()), "sensor_id" : sensor.id, "sensor_type" : "DS18B20", "temp_C" : tempC}
-        msg_json = json.dumps(msg_payload)
-        syslog.syslog(msg_json)
-        client.publish(MQTT_topic, msg_json)
+        log("DEBUG : in the sensor loop ... " + sensor.id)
+        try :
+            tempC = sensor.get_temperature()
+            sensor_ok = True
+        except:
+            log("ERROR : Undefined sensor error. Sensor ID " + sensor.id)
+            sensor_ok = False
+            continue                      # ... and try the remaining sensors and loop and try again.
+        if sensor_ok:
+            msg_payload = {"timestamp" : str(datetime.now()), "sensor_id" : sensor.id, "sensor_type" : "DS18B20", "temp_C" : tempC}
+            msg_json = json.dumps(msg_payload)
+            log(msg_json)
+            client.publish(MQTT_topic, msg_json)
+
     return()
 
 def get_pH():
     return()
 
 if __name__ == '__main__':
+    startup(10)
+    log("INFO : logging interval " + str(INTERVAL) + " seconds")
     syslog.openlog(logoption=syslog.LOG_PID)
-    syslog.syslog("Temperature logging started ...")
+    log("INFO : Temperature logging started ...")
     Connected = False                                   #global variable for the state of the connection
     broker_address= MQTT_broker
     port = MQTT_port
@@ -58,15 +95,18 @@ if __name__ == '__main__':
     client.on_connect= on_connect                      #attach function to callback
     client.connect(broker_address, port=port)          #connect to broker
     client.loop_start()                                #start the loop
-    
+    log("DEBUG : Connected? " + str(Connected))
     while Connected != True:    #Wait for connection
         time.sleep(0.1)
+    log("DEBUG : Connected? " + str(Connected))
+    
     try :
         while True:
             get_Temp(client)
             get_pH()
             time.sleep(INTERVAL)
     except KeyboardInterrupt:
-        print("Terminating connection to MQTT broker")
+        log("INFO : Terminating connection to MQTT broker")
+        log("INFO : Terminated.")
         client.disconnect()
         client.loop_stop()
